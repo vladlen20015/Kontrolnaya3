49dbde5cf8f660a57248e9afccb61dbb4e30cd08
vladlen20015 <vladlena20015@gmail.com>
2020-12-01 17:00:17 +0900

    Романова Владлена контрольная 3

---
+++ kontrol3/src/kontr3/Zadanie1.java
@@ -0,0 +1,8 @@
+package kontr3;
+
+public class Zadanie1 {
+    public boolean chec(int a, int b){
+        int sum=a+b;
+        return (10 <= sum) && (sum <= 20);
+    }
+}
---
+++ kontrol3/src/kontr3/Zadanie2.java
@@ -0,0 +1,12 @@
+package kontr3;
+
+public class Zadanie2 {
+    public static void main(String[] args){
+        int[] arr=new int[]{0,3,6,9,12,15,18,21};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
+
+
